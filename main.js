34fb09cedcf3d7976d1e037471c7473e57deff01
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-08-01 22:48:13 +0100

    corrected

--- main.js
+++ main.js
@@ -2,12 +2,20 @@ let btn = document.querySelectorAll('.btn-container button')
 let card2 = document.querySelector('.card-2')
 let Card = document.querySelector('.card')
 let span = document.querySelector('span')
+let submit = document.querySelector('.submit')
 
 Array.from(btn).forEach(btn =>{
     btn.addEventListener('click' , e => {
         const rating = e.target.textContent
         span.textContent = rating
-        card2.classList.remove('hide')
-        Card.classList.add('hide')
+       // card2.classList.remove('hide')
+        //Card.classList.add('hide')
     })
+})
+submit.addEventListener('click', function(e){
+    //console.log('fauzee');
+    //const rating = e.target.textContent
+    //console.log(rating);
+    card2.classList.remove('hide')
+        Card.classList.add('hide')
 })
